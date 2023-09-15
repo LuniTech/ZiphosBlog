382a0de7cc80babaa269353836283e95e6b596b6
Innocent Lunika <67061471+LuniTech@users.noreply.github.com>
2023-09-15 16:00:22 +0200

    File upload

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const AWS = require('aws-sdk');
 
 // Configure AWS SDK with your credentials
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_IDD,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEYY,
+  region: process.env.AWS_REGIONN,
 });
 
 const polly = new AWS.Polly();
