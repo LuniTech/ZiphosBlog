af14ddfe8ff83149f96b63a0638e90ff1676efd9
Innocent Lunika <67061471+LuniTech@users.noreply.github.com>
2023-09-15 15:38:34 +0200

    Files Upload
    Added AWS

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+/*Added stuff*/
+
 const express = require("express");
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 require("dotenv").config();
 const app = express();
+//Added code:  ==========================
+const AWS = require('aws-sdk');
+
+// Configure AWS SDK with your credentials
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const polly = new AWS.Polly();
+
+async function generatePollyAudio(text) {
+  // Use Amazon Polly to generate audio from text
+  // Implement the Polly audio generation logic here
+  // Return the URL or audio data
+}
+
+module.exports = { generatePollyAudio };
+//END=======================
+
 let Article = require("./models/article")
 let uri = process.env.MONGO_URI;
 mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
